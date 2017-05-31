fe7a685612815baf5bafb7f0eebb789b87895e05
Brian Chun <BChun37@yahoo.com>
2017-05-31 12:12:05 -0700

    Initial Commit

---
+++ TableViewController.swift
@@ -0,0 +1,144 @@
+//
+//  TableViewController.swift
+//  CoreDataDemo
+//
+//  Created by Brian Chun on 5/4/17.
+//  Copyright © 2017 Brian Chun. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TableViewController: UITableViewController {
+
+    let firstNames = ["Jane", "John", "Stephen", "Stacy", "Taylor", "Alex", "Eren"]
+    let lastNames = ["White", "Black", "Fox", "Jones", "King", "McQueen", "Yeager"]
+    let ages = [25, 26, 20, 30, 27, 28, 23]
+    var people = [NSManagedObject]()
+    
+    @IBAction func addButtonWasTapped(_ sender: UIBarButtonItem) {
+        
+        let randomFirstName = firstNames[Int(arc4random_uniform(UInt32(firstNames.count)))]
+        let randomLastName = lastNames[Int(arc4random_uniform(UInt32(lastNames.count)))]
+        let randomAge = ages[Int(arc4random_uniform(UInt32(ages.count)))]
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Person", in: context)
+        
+        let person = NSManagedObject(entity: entity!, insertInto: context)
+        person.setValue(randomFirstName, forKey: "firstName")
+        person.setValue(randomLastName, forKey: "lastName")
+        person.setValue(randomAge, forKey: "age")
+        appDelegate.saveContext()
+        people.append(person)
+        
+        self.tableView.reloadData()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let person = people[indexPath.row]
+        let firstName = person.value(forKey: "firstName")
+        let lastName = person.value(forKey: "lastName")
+        let age = person.value(forKey: "age")
+        cell.textLabel?.text = "\(firstName!) \(lastName!) age \(age!)"
+        
+        return cell
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Person")
+        
+        do {
+            let results = try managedContext.fetch(fetchRequest)
+            people = results as! [NSManagedObject]
+        } catch let error as NSError {
+            print("Fetching Error: \(error.userInfo)")
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
